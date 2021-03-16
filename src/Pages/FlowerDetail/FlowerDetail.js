d42ea7e12187a5fddcb2d8c7204ae2c605794582
sunaaank <suna.namgung@gmail.com>
2021-03-16 14:39:52 +0900

    Add : detail 컴포넌트 CSS 기본 셋팅 완료, CSS 이벤트 속성 작업중

--- src/Pages/FlowerDetail/FlowerDetail.js
+++ src/Pages/FlowerDetail/FlowerDetail.js
@@ -5,78 +5,145 @@ class FlowerDetail extends Component {
   render() {
     return (
       <section className="detail-container">
-        <div className="category-nav">
-          <span>HOME</span>
-          <span>꽃 정기구독</span>
-          <span>가벼운 S 꽃구독</span>
-        </div>
+        <ul className="category-nav">
+          <li>HOME</li>
+          <li>꽃 정기구독</li>
+          <li>가벼운 S 꽃구독</li>
+        </ul>
         <div className="detail-contents">
           <div className="product-thumnail">
             <img
               alt="꽃 이미지"
-              src="https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Ft1.daumcdn.net%2Fcfile%2Ftistory%2F99DD0D485BAC757D2D"
+              src="https://lh3.googleusercontent.com/proxy/D36gvJKAbmz-6WstI8gQRBOQVfu1IxXTczck8KN9Z3BJ9v1HVqi2zoy_fzxxSRGVMmnVcgiRQcqmYEuZ4lkP84ZYEpEYdZwObSDm_TMrHPaTTJlz0i6nuQDdtG86xmfMPM-mjVxRz3DUv8HVwN4ImfYD8PhanNwCmJ6dG7GjWlDFABjMlw2lwgfhgHWd4AEHVK81rYze_TrvjfAz37p2wZ4NwZuAS2LDJTjrKjlEMzkRzfwm9u4Lu5E9Q9XA0-vuZx9MLbwP_WYnkeq4gqsaG9cWTFZiCK5krMDnWBPyoidj1sjvGQgJaqB0q4ADSjIZRs9hGV4PE3maHv5uR55kNt0"
               className="thumnail"
             />
             <div className="detail-slider">
               <img
                 alt="꽃 미니 썸네일1"
-                src="https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Ft1.daumcdn.net%2Fcfile%2Ftistory%2F99DD0D485BAC757D2D"
+                src="https://i.pinimg.com/originals/37/d3/56/37d3567b0aacc4d36b4f35e2bf91f7df.jpg"
                 className="thumnail-mini"
               />
               <img
                 alt="꽃 미니 썸네일2"
-                src="https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Ft1.daumcdn.net%2Fcfile%2Ftistory%2F99DD0D485BAC757D2D"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3yfsNpsJUIk_Rcywam0WEW3zuTk_yuELzRQ&usqp=CAU"
                 className="thumnail-mini"
               />
               <img
                 alt="꽃 미니 썸네일3"
-                src="https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Ft1.daumcdn.net%2Fcfile%2Ftistory%2F99DD0D485BAC757D2D"
+                src="https://lh3.googleusercontent.com/proxy/D36gvJKAbmz-6WstI8gQRBOQVfu1IxXTczck8KN9Z3BJ9v1HVqi2zoy_fzxxSRGVMmnVcgiRQcqmYEuZ4lkP84ZYEpEYdZwObSDm_TMrHPaTTJlz0i6nuQDdtG86xmfMPM-mjVxRz3DUv8HVwN4ImfYD8PhanNwCmJ6dG7GjWlDFABjMlw2lwgfhgHWd4AEHVK81rYze_TrvjfAz37p2wZ4NwZuAS2LDJTjrKjlEMzkRzfwm9u4Lu5E9Q9XA0-vuZx9MLbwP_WYnkeq4gqsaG9cWTFZiCK5krMDnWBPyoidj1sjvGQgJaqB0q4ADSjIZRs9hGV4PE3maHv5uR55kNt0"
                 className="thumnail-mini"
               />
+
               <img
                 alt="꽃 미니 썸네일4"
-                src="https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Ft1.daumcdn.net%2Fcfile%2Ftistory%2F99DD0D485BAC757D2D"
+                src="https://lh3.googleusercontent.com/proxy/D36gvJKAbmz-6WstI8gQRBOQVfu1IxXTczck8KN9Z3BJ9v1HVqi2zoy_fzxxSRGVMmnVcgiRQcqmYEuZ4lkP84ZYEpEYdZwObSDm_TMrHPaTTJlz0i6nuQDdtG86xmfMPM-mjVxRz3DUv8HVwN4ImfYD8PhanNwCmJ6dG7GjWlDFABjMlw2lwgfhgHWd4AEHVK81rYze_TrvjfAz37p2wZ4NwZuAS2LDJTjrKjlEMzkRzfwm9u4Lu5E9Q9XA0-vuZx9MLbwP_WYnkeq4gqsaG9cWTFZiCK5krMDnWBPyoidj1sjvGQgJaqB0q4ADSjIZRs9hGV4PE3maHv5uR55kNt0"
                 className="thumnail-mini"
               />
+
               <img
                 alt="꽃 미니 썸네일5"
-                src="https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Ft1.daumcdn.net%2Fcfile%2Ftistory%2F99DD0D485BAC757D2D"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTw1qCVTfj9IReqU0VvafDxKsC9Xukz44hqWA&usqp=CAU"
                 className="thumnail-mini"
               />
             </div>
           </div>
           <div className="product-detail-right">
             <div className="detail-title">
-              <span>꽃이 일상이 되는 가벼운 시작</span>
-              <h2 className="strong">가벼운 S 꽃구독</h2>
-              <h2>17,900원</h2>
+              <span className="sub-title">꽃이 일상이 되는 가벼운 시작</span>
+              <h2 className="title strong">가벼운 S 꽃구독</h2>
+              <h2 className="title">17,900원</h2>
             </div>
             <div className="detail-info">
-              <span>정기구독 전상품, 무료배송!</span>
+              <span className="contents">정기구독 전상품, 무료배송!</span>
               <div className="detail-option">
                 <div className="subscribe-option">
-                  <span>구독옵션</span>
-                  <button type="button" className="opener" />
-                  <ul className="options">
-                    <li className="option"> 정기구독(2주마다 자동결제)</li>
-                  </ul>
-                  <div className="detail-amount">
-                    <button type="button" className="btn-amount">
-                      -
-                    </button>
-                    <span>수량</span>
-                    <button type="button" className="btn-amount">
-                      +
-                    </button>
-                  </div>
+                  <table>
+                    <thead>
+                      <th></th>
+                      <th></th>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>
+                          <th></th>
+                          <span className="contents">구독옵션</span>
+                        </td>
+                        <td>
+                          <input
+                            type="text"
+                            className="selected"
+                            readonly
+                            placeholder="구독기간을 선택해주세요"
+                          />
+                          <button type="button" className="opener" />
+                          <ul className="options">
+                            <li className="option">
+                              정기구독(2주마다 자동결제)
+                            </li>
+                          </ul>
+                        </td>
+                      </tr>
+                      <tr>
+                        <th>
+                          <span className="contents">수량</span>
+                        </th>
+                        <td>
+                          <div className="detail-amount">
+                            <button type="button" className="btn-amount">
+                              -
+                            </button>
+                            <span>1</span>
+                            <button type="button" className="btn-amount">
+                              +
+                            </button>
+                          </div>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <th></th>
+                          <span className="contents">편지 추가</span>
+                        </td>
+                        <td>
+                          <div className="letter-checkboxes">
+                            <div className="letter-checkbox">
+                              <input
+                                type="radio"
+                                className="checked"
+                                data-value="True"
+                              />
+                              <span className="contents">
+                                추가할게요 (+2,500원)
+                              </span>
+                            </div>
+                            <div className="letter-checkbox">
+                              <input
+                                type="radio"
+                                className="checked"
+                                data-value="False"
+                              />
+                              <span className="contents">
+                                추가하지 않을게요
+                              </span>
+                            </div>
+                          </div>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
                   <div className="detail-price">
-                    <span>상품 가격</span>
-                    <p>구독 기간을 선택해주세요</p>
+                    <div>
+                      <span className="contents">상품 가격</span>
+                      <p className="sub-contents">구독 기간을 선택해주세요</p>
+                    </div>
                     <span>35000원</span>
                   </div>
                   <div className="total-price">
-                    <p>총 주문금액</p>
-                    <h2>35000원</h2>
+                    <p className="shipping-free">무료배송</p>
+                    <div>
+                      <p className="sub-contents">총 주문금액</p>
+                      <h2 className="title">35000원</h2>
+                    </div>
                   </div>
                   <div className="detail-buttons">
                     <button type="button" className="detail-btn cart">
