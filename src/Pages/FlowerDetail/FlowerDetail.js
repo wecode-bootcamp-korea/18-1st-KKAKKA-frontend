ecd3376c8e83e53d53518669d5f449895e77580f
sunaaank <suna.namgung@gmail.com>
2021-03-16 20:02:27 +0900

    Fix : detail 컴포넌트 레이아웃 디테일 수정

--- src/Pages/FlowerDetail/FlowerDetail.js
+++ src/Pages/FlowerDetail/FlowerDetail.js
@@ -69,26 +69,24 @@ class FlowerDetail extends Component {
                       <th></th>
                     </thead>
                     <tbody>
-                      <tr>
+                      <tr className="option-row-1">
                         <th>
                           <span className="contents">구독옵션</span>
                         </th>
                         <td>
-                          <input
-                            type="text"
-                            className="selected"
-                            readonly
-                            placeholder="구독기간을 선택해주세요"
-                          />
-                          <button type="button" className="opener" />
-                          <ul className="options">
-                            <li className="option">
+                          <select>
+                            <label for="option">구독 옵션</label>
+                            <option className="selected">
+                              구독기간을 선택해주세요
+                            </option>
+                            <option className="option">
                               정기구독(2주마다 자동결제)
-                            </li>
-                          </ul>
+                            </option>
+                            <option className="option">1회 무료 체험</option>
+                          </select>
                         </td>
                       </tr>
-                      <tr>
+                      <tr className="option-row-2">
                         <th>
                           <span className="contents">수량</span>
                         </th>
@@ -104,7 +102,7 @@ class FlowerDetail extends Component {
                           </div>
                         </td>
                       </tr>
-                      <tr>
+                      <tr className="option-row-3">
                         <th>
                           <span className="contents">편지 추가</span>
                         </th>
