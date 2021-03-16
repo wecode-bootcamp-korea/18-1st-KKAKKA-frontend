b3f1b4002472c235d4b5e54889a3549bff6071d9
Geunhwa Lee <geunhwa.lee.01@gmail.com>
2021-03-16 16:30:11 +0900

    Add: 로그인 페이지 레이아웃 완료

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,7 +1,24 @@
 import React, { Component } from "react";
+import "./Login.scss";
+
 class Login extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div className="login">
+        <div className="loginBox">
+          <h1 className="loginText">로그인</h1>
+          <form className="loginField">
+            <input className="inputId" placeholder="아이디(이메일)" />
+            <input className="inputPwd" placeholder="비밀번호" />
+            <div className="inputBtnBuffer"></div>
+            <button className="loginBtn">로그인</button>
+            <div className="signUpArea">
+              <button className="signUpBtn">회원가입</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
   }
 }
 export default Login;
