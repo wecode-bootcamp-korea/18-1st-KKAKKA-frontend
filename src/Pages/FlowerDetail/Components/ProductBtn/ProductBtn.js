ba08765493da4755617d59c88708dae5732ef81b
sunaaank <suna.namgung@gmail.com>
2021-03-17 11:13:36 +0900

    Add : button, input, textarea 포커스, 액티브 스타일 reset, Detail 페이지 버튼 Link 연결

--- src/Pages/FlowerDetail/Components/ProductBtn/ProductBtn.js
+++ src/Pages/FlowerDetail/Components/ProductBtn/ProductBtn.js
@@ -1,17 +1,29 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import "./ProductBtn.scss";
 
 class ProductBtn extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isLogin: true,
+    };
+  }
+
   render() {
-    // const { } = this.props;
+    const { isLogin } = this.state;
     return (
       <div className="detail-buttons">
-        <button type="button" className="detail-btn cart">
-          장바구니
-        </button>
-        <button type="button" className="detail-btn order">
-          바로 구독 신청
-        </button>
+        <Link to="/cart">
+          <button type="button" className="detail-btn cart">
+            장바구니
+          </button>
+        </Link>
+        <Link to={isLogin ? "/order" : "/login"}>
+          <button type="button" className="detail-btn order">
+            바로 구독 신청
+          </button>
+        </Link>
       </div>
     );
   }
