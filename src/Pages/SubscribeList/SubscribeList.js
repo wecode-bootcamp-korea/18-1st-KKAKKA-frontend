0a3574131d54300a7d473b973fafe4c12a0de9bd
sunaaank <suna.namgung@gmail.com>
2021-03-17 16:26:50 +0900

    Add : list 페이지 하위로 detail 페이지 넣음, router경로 변경

--- src/Pages/SubscribeList/SubscribeList.js
+++ src/Pages/SubscribeList/SubscribeList.js
@@ -1,7 +1,40 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import SubscribeDetail from "./SubscribeDetail/SubscribeDetail";
+
 class SubscribeList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      subscribeList: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("/data/subscribeData.json")
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          subscribeList: data,
+        });
+      });
+  }
+
   render() {
-    return <div></div>;
+    const { subscribeList } = this.state;
+    return (
+      <div>
+        <SubscribeDetail
+          key={subscribeList.id}
+          itemComment={subscribeList.itemComment}
+          prouductTitle={subscribeList.prouductTitle}
+          price={subscribeList.price}
+          contents={subscribeList.contents}
+          productImg={subscribeList.productImg}
+        />
+        <Link></Link>
+      </div>
+    );
   }
 }
 export default SubscribeList;
