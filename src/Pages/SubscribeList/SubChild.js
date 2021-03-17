72a2966c02b285a4922cd7102c611fb4ab802891
sunaaank <suna.namgung@gmail.com>
2021-03-17 20:39:45 +0900

    Add : mock data, mapping 추가

---
+++ src/Pages/SubscribeList/SubChild.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+class SubChild extends Component {
+  render() {
+    const {
+      itemComment,
+      productTitle,
+      price,
+      contents,
+      productImg,
+    } = this.props;
+    return (
+      <article className="sublist-box">
+        <div className="sub-image-box">
+          <Link to="./SubscribeDetail">
+            <img
+              alt="정기구독 상품1"
+              src={productImg.img1}
+              className="sub-image"
+            />
+          </Link>
+        </div>
+        <div className="detail-title">
+          <span className="sub-title">{itemComment}</span>
+          <Link to="./SubscribeDetail">
+            <h2 className="title strong">{productTitle}</h2>
+          </Link>
+          <h2 className="title price">{price}</h2>
+          <div className="content-box">
+            {contents.split("\n").map(line => {
+              return (
+                <span className="detail-content">
+                  {line}
+                  <br />
+                </span>
+              );
+            })}
+          </div>
+
+          <span className="shipping-free">무료배송</span>
+        </div>
+      </article>
+    );
+  }
+}
+export default SubChild;
--- src/Pages/SubscribeList/SubscribeList.js
+++ src/Pages/SubscribeList/SubscribeList.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 import SubscribeDetail from "./SubscribeDetail/SubscribeDetail";
+import SubChild from "./SubChild";
 import "./SubscribeList.scss";
 
 class SubscribeList extends Component {
@@ -34,142 +34,26 @@ class SubscribeList extends Component {
           </button>
         </nav>
 
-        <article className="sublist-box">
-          <div className="sub-image-box">
-            <Link to="./SubscribeDetail">
-              <img
-                alt="정기구독 상품1"
-                src="https://user-images.githubusercontent.com/46774456/111404908-eff67680-8712-11eb-8f25-8ae36c32578e.png"
-                className="sub-image"
-              />
-            </Link>
-          </div>
-          <div className="detail-title">
-            <span className="sub-title">꽃이 일상이 되는 가벼운 시작</span>
-            <Link to="./SubscribeDetail">
-              <h2 className="title strong">가벼운 S 꽃구독</h2>
-            </Link>
-            <h2 className="title price">17,900</h2>
-            <div className="detail-discount">
-              <span className="discountPer">10</span>
-              <span className="oldPrice sub-title">17,900</span>
-              <h2 className="title price">17,900</h2>
-            </div>
-            <div className="content-box">
-              <span className="detail-content">
-                2주에 한 번, 꽃이 일상이 되는 가벼운 시작
-              </span>
-              <span className="detail-content">
-                작지만 그 계절 가장 예쁜 꽃으로 만들었어요.
-              </span>
-              <span className="detail-content">
-                나에게, 사랑하는 사람에게 작은 꽃다발이 주는 행복을 전해보세요.
-              </span>
-            </div>
-
-            <span className="shipping-free">무료배송</span>
-          </div>
-          <SubscribeDetail
-            key={subscribeList.id}
-            itemComment={subscribeList.itemComment}
-            prouductTitle={subscribeList.prouductTitle}
-            price={subscribeList.price}
-            contents={subscribeList.contents}
-            productImg={subscribeList.productImg}
-          />
-          <Link></Link>
-        </article>
-
-        <article className="sublist-box">
-          <div className="sub-image-box">
-            <Link to="./SubscribeDetail">
-              <img
-                alt="정기구독 상품1"
-                src="https://user-images.githubusercontent.com/46774456/111404908-eff67680-8712-11eb-8f25-8ae36c32578e.png"
-                className="sub-image"
-              />
-            </Link>
-          </div>
-          <div className="detail-title">
-            <span className="sub-title">꽃이 일상이 되는 가벼운 시작</span>
-            <Link to="./SubscribeDetail">
-              <h2 className="title strong">가벼운 S 꽃구독</h2>
-            </Link>
-            <h2 className="title price">17,900</h2>
-            <div className="detail-discount">
-              <span className="discountPer">10</span>
-              <span className="oldPrice sub-title">17,900</span>
-              <h2 className="title price">17,900</h2>
-            </div>
-            <div className="content-box">
-              <span className="detail-content">
-                2주에 한 번, 꽃이 일상이 되는 가벼운 시작
-              </span>
-              <span className="detail-content">
-                작지만 그 계절 가장 예쁜 꽃으로 만들었어요.
-              </span>
-              <span className="detail-content">
-                나에게, 사랑하는 사람에게 작은 꽃다발이 주는 행복을 전해보세요.
-              </span>
-            </div>
-
-            <span className="shipping-free">무료배송</span>
-          </div>
-          <SubscribeDetail
-            key={subscribeList.id}
-            itemComment={subscribeList.itemComment}
-            prouductTitle={subscribeList.prouductTitle}
-            price={subscribeList.price}
-            contents={subscribeList.contents}
-            productImg={subscribeList.productImg}
-          />
-          <Link></Link>
-        </article>
-        <article className="sublist-box">
-          <div className="sub-image-box">
-            <Link to="./SubscribeDetail">
-              <img
-                alt="정기구독 상품1"
-                src="https://user-images.githubusercontent.com/46774456/111404908-eff67680-8712-11eb-8f25-8ae36c32578e.png"
-                className="sub-image"
-              />
-            </Link>
-          </div>
-          <div className="detail-title">
-            <span className="sub-title">꽃이 일상이 되는 가벼운 시작</span>
-            <Link to="./SubscribeDetail">
-              <h2 className="title strong">가벼운 S 꽃구독</h2>
-            </Link>
-            <h2 className="title price">17,900</h2>
-            <div className="detail-discount">
-              <span className="discountPer">10</span>
-              <span className="oldPrice sub-title">17,900</span>
-              <h2 className="title price">17,900</h2>
-            </div>
-            <div className="content-box">
-              <span className="detail-content">
-                2주에 한 번, 꽃이 일상이 되는 가벼운 시작
-              </span>
-              <span className="detail-content">
-                작지만 그 계절 가장 예쁜 꽃으로 만들었어요.
-              </span>
-              <span className="detail-content">
-                나에게, 사랑하는 사람에게 작은 꽃다발이 주는 행복을 전해보세요.
-              </span>
-            </div>
-
-            <span className="shipping-free">무료배송</span>
-          </div>
-          <SubscribeDetail
-            key={subscribeList.id}
-            itemComment={subscribeList.itemComment}
-            prouductTitle={subscribeList.prouductTitle}
-            price={subscribeList.price}
-            contents={subscribeList.contents}
-            productImg={subscribeList.productImg}
-          />
-          <Link></Link>
-        </article>
+        {subscribeList.map(sub => {
+          return (
+            <SubChild
+              key={sub.id}
+              itemComment={sub.itemComment}
+              productTitle={sub.prouductTitle}
+              price={sub.price}
+              contents={sub.contents}
+              productImg={sub.productImg}
+            />
+          );
+        })}
+        <SubscribeDetail
+          key={subscribeList.id}
+          itemComment={subscribeList.itemComment}
+          prouductTitle={subscribeList.prouductTitle}
+          price={subscribeList.price}
+          contents={subscribeList.contents}
+          productImg={subscribeList.productImg}
+        />
       </section>
     );
   }
