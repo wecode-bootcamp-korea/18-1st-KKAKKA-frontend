691069a4095d6a5173ee915d47199a592bb55343
sunaaank <suna.namgung@gmail.com>
2021-03-18 15:53:54 +0900

    Add : 구독리스트_이용방법 탭 요소_컴포넌트 분리

--- src/Pages/SubscribeList/SubscribeList.js
+++ src/Pages/SubscribeList/SubscribeList.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import SubCard from "./SubCard";
+import SubCard from "./SubCard/SubCard";
+import SubStart from "./SubStart/SubStart";
 import "./SubscribeList.scss";
 
 class SubscribeList extends Component {
@@ -25,122 +26,42 @@ class SubscribeList extends Component {
       });
   }
   // {this.state.currentId === 1 && <SubCard />};
-  // {this.state.currentId === 2 && <SubCard />};
+  // {this.state.currentId === 2 && <SubStart />};
   render() {
     const { subscribeList } = this.state;
     return (
-      <>
-        <section className="sublistContainer">
-          <nav className="subToggle">
-            <button
-              className="toggleBtn"
-              onclick={() => this.clickHandler(1)}
-              type="button"
-            >
-              <h4 className="toggle">정기구독 상품</h4>
-            </button>
-            <button
-              className="toggleBtn"
-              onclick={() => this.clickHandler(2)}
-              type="button"
-            >
-              <h4 className="toggle">이용 방법</h4>
-            </button>
-          </nav>
-          {subscribeList.map(sub => {
-            return (
-              <SubCard
-                key={sub.id}
-                id={sub.id}
-                itemComment={sub.itemComment}
-                productTitle={sub.prouductTitle}
-                price={sub.price}
-                contents={sub.contents}
-                productImg={sub.productImg}
-              />
-            );
-          })}
-        </section>
-        <section className="subStart">
-          <ol>
-            <li className="subSteplist">
-              <div className="steplistImg">
-                <img
-                  alt="정기구독 방법 step 1"
-                  src="https://user-images.githubusercontent.com/46774456/111411544-1a016600-871e-11eb-91a4-4ef2f03121f0.png"
-                  className="subStep"
-                />
-              </div>
-              <strong className="stepNum">01</strong>
-              <span>
-                정기구독 상품 중{" "}
-                <strong>
-                  원하시는
-                  <br />꽃 사이즈
-                </strong>
-                를 선택해주세요.
-              </span>
-            </li>
-            <li className="subSteplist">
-              <div className="steplistImg">
-                <img
-                  alt="정기구독 방법 step 2"
-                  src="https://user-images.githubusercontent.com/46774456/111411544-1a016600-871e-11eb-91a4-4ef2f03121f0.png"
-                  className="subStep"
-                />
-              </div>
-              <strong className="stepNum">02</strong>
-              <span>
-                꽃을{" "}
-                <strong>
-                  구독하고 싶은
-                  <br />총 기간
-                </strong>
-                을 선택해주세요.
-              </span>
-            </li>
-            <li className="subSteplist">
-              <div className="steplistImg">
-                <img
-                  alt="정기구독 방법 step 3"
-                  src="https://user-images.githubusercontent.com/46774456/111411544-1a016600-871e-11eb-91a4-4ef2f03121f0.png"
-                  className="subStep"
-                />
-              </div>
-              <strong className="stepNum">03</strong>
-              <span>
-                꽃을 <strong>받고 싶은 요일</strong>을
-                <br />
-                선택해주세요.
-              </span>
-            </li>
-            <li className="subSteplist">
-              <div className="steplistImg">
-                <img
-                  alt="정기구독 방법 step 4"
-                  src="https://user-images.githubusercontent.com/46774456/111411544-1a016600-871e-11eb-91a4-4ef2f03121f0.png"
-                  className="subStep"
-                />
-              </div>
-              <strong className="stepNum">04</strong>
-              <span>
-                <strong>
-                  2주에 한번 그 계절 가장
-                  <br />
-                  이쁜 꽃
-                </strong>
-                으로 찾아갈게요.
-              </span>
-            </li>
-          </ol>
-          <div className="subTip">
-            <span className="changeDate">
-              혹시, 일정을 변경하고 싶으신가요? 마이페이지 혹은 고객센터를 통해
-              미리 일정을 변경하실 수 있습니다.
-            </span>
-          </div>
-        </section>
-      </>
+      <section className="sublistContainer">
+        <nav className="subToggle">
+          <button
+            className="toggleBtn"
+            onclick={() => this.clickHandler(1)}
+            type="button"
+          >
+            <h4 className="toggle">정기구독 상품</h4>
+          </button>
+          <button
+            className="toggleBtn"
+            onclick={() => this.clickHandler(2)}
+            type="button"
+          >
+            <h4 className="toggle">이용 방법</h4>
+          </button>
+        </nav>
+        {subscribeList.map(sub => {
+          return (
+            <SubCard
+              key={sub.id}
+              id={sub.id}
+              itemComment={sub.itemComment}
+              productTitle={sub.prouductTitle}
+              price={sub.price}
+              contents={sub.contents}
+              productImg={sub.productImg}
+            />
+          );
+        })}
+        <SubStart />
+      </section>
     );
   }
 }
