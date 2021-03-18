0443b3df71e1c3f0e0a3846de8d23f1ff435004e
sunaaank <suna.namgung@gmail.com>
2021-03-18 14:16:45 +0900

    Add : ProductInfo에 props 추가

--- src/Pages/FlowerDetail/Components/ProductInfo/ProductInfo.js
+++ src/Pages/FlowerDetail/Components/ProductInfo/ProductInfo.js
@@ -3,16 +3,16 @@ import "./ProductInfo.scss";
 
 class ProductInfo extends Component {
   render() {
-    // const { } = this.props;
+    const { itemComment, productTitle, price } = this.props;
     return (
       <div className="detail-title">
-        <span className="sub-title">꽃이 일상이 되는 가벼운 시작</span>
-        <h2 className="title strong">가벼운 S 꽃구독</h2>
-        <h2 className="title price">17,900</h2>
+        <span className="sub-title">{itemComment}</span>
+        <h2 className="title strong">{productTitle}</h2>
+        <h2 className="title price">{price}</h2>
         <div className="detail-discount">
           <span className="discountPer">10</span>
           <span className="oldPrice sub-title">17,900</span>
-          <h2 className="title price">17,900</h2>
+          <h2 className="title price">{price}</h2>
         </div>
       </div>
     );
