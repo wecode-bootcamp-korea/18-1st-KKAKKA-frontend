70531a5eddbd7b413a6f7cffe5855a4cdd030598
sunaaank <suna.namgung@gmail.com>
2021-03-20 02:32:52 +0900

    Add : Nav 기본 레이아웃 완료, 스크롤 반응형 구현 전

---
+++ src/Pages/Main/Components/Nav/Nav.js
@@ -0,0 +1,88 @@
+import React from "react";
+import { Link, withRouter } from "react-router-dom";
+import { BsPerson, BsBucket } from "react-icons/bs";
+import "./Nav.scss";
+
+class Nav extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      // api 연결 이후 fetch 로그인 상태 인증으로 로직 변경
+      isLogin: true,
+    };
+  }
+
+  goToMypage = () => {
+    if (this.state.isLogin) {
+      this.props.history.push("/mypage");
+    } else {
+      this.props.history.push("/login");
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="topBanner">
+          <span className="banner">
+            🍪 까까 팀이 궁금하다면? 🍪 노션 페이지 바로가기💨
+          </span>
+        </div>
+        <div className="navInfo">
+          <li className="navUser">
+            <Link to="./login">로그인</Link>
+          </li>
+          <li className="navUser">
+            <Link to="./signup">회원가입</Link>
+            <p>(10000 포인트 지급!)</p>
+          </li>
+        </div>
+        <nav className="navbar navUp">
+          <div className="navLogo">
+            <Link to="./main">KKAKKA</Link>
+          </div>
+
+          <ul className="navMenu">
+            <li>
+              <Link to="./subscribe-list">쿠키 정기구독</Link>
+            </li>
+            <li>
+              <Link to="./flower-list">쿠키쿠키</Link>
+            </li>
+            <li>
+              <Link to="/flower-class-list">쿠킹클래스</Link>
+            </li>
+            <li>
+              <Link to="./main">쿠킹샵</Link>
+            </li>
+            <li>
+              <Link to="./main">이벤트</Link>
+            </li>
+          </ul>
+
+          <ul className="navIcons">
+            <li>
+              <BsPerson
+                value={{ className: "navIcon" }}
+                size="2em"
+                verticalAlign="middle"
+                onClick={this.goToMypage}
+              />
+            </li>
+            <li>
+              <Link to="./cart">
+                <BsBucket
+                  value={{ className: "navIcon" }}
+                  size="2em"
+                  verticalAlign="middle"
+                />
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default withRouter(Nav);
--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -1,7 +1,15 @@
 import React, { Component } from "react";
+import Nav from "./Components/Nav/Nav";
+import "./Main.scss";
+
 class Main extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div className="container">
+        <Nav className="nav" />
+        <section className="test" />
+      </div>
+    );
   }
 }
 export default Main;
