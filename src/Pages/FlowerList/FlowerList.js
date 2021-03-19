18f13b3dacd9675c57ff3a4e01a1fe0dd2659cde
Young <dudrbs10400@gmail.com>
2021-03-19 10:15:03 +0900

    Add: flowerlist 목데이터 구현

--- src/Pages/FlowerList/FlowerList.js
+++ src/Pages/FlowerList/FlowerList.js
@@ -1,7 +1,130 @@
 import React, { Component } from "react";
+import ListDatas from "./ListDatas";
+import "./FlowerList.scss";
+
 class FlowerList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      listDatas: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("/data/FlowerList.json")
+      .then(res => res.json())
+      .then(data => {
+        //console.log(data);
+        this.setState({
+          listDatas: data,
+        });
+      });
+  }
+
+  // startLowPrice = (a,b) => {
+  //   this.setState({
+  //     listDatas: this.state.listDatas.sort(a.{resultPrice}-(b.{resultPrice}),
+
+  //   });
+  // };
+
+  // fetch('https://jsonplaceholder.typicode.com/posts').then((res) => res.json())
+  // .then((data) => {
+  //     data.sort((a, b) => a.userId - b.userId);
+  //     this.setState({data: data});
+
+  // });
+
+  //   listDatas.sort((a, b) => {
+  //     return parseFloat(a.price) - parseFloat(b.price);
+  // });
+  // test.sort(function(a,b) {
+
+  //   return parseFloat(a.order) - parseFloat(b.order);
+
+  // });
+
+  // var sortingNumbers1 = (a,b) => {
+
+  // 	return (a-b);
+
+  // }
+
   render() {
-    return <div></div>;
+    const { listDatas } = this.state;
+
+    return (
+      <div className="flowerListMain">
+        <button class="startLowPrice" onClick={this.startLowPrice}>
+          가격낮은순
+        </button>
+
+        <div class="sort">
+          <div class="select_sort_product">
+            <span class="blind">상품 정렬 방식 :</span>
+            <span class="selected">신상품순</span>
+            <button type="button" class="opener">
+              <span class="blind">정렬방식 변경</span>
+            </button>
+            <ul class="other">
+              <li class="type">
+                <button
+                  type="button"
+                  class="change"
+                  data-text="추천순"
+                  data-code="PICK"
+                >
+                  추천순<span class="blind">정렬</span>
+                </button>
+              </li>
+              <li class="type">
+                <button
+                  type="button"
+                  class="change"
+                  data-text="인기순"
+                  data-code="BEST"
+                >
+                  인기순<span class="blind">정렬</span>
+                </button>
+              </li>
+              <li class="type is_select">
+                <button
+                  type="button"
+                  class="change"
+                  data-text="신상품순"
+                  data-code="NEW"
+                >
+                  신상품순<span class="blind">정렬</span>
+                </button>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="list">
+          {listDatas.map(list => {
+            return (
+              <ListDatas
+                key={list.id}
+                id={list.id}
+                itemComment={list.itemComment}
+                prouductTitle={list.prouductTitle}
+                price={list.price}
+                isDiscount={list.isDiscount}
+                discountPer={list.discountPer}
+                resultPrice={list.resultPrice}
+                productLimit={list.productLimit}
+                size={list.size}
+                updateDate={list.updateDate}
+                productImg={list.productImg}
+                isFreeShipping={list.isFreeShipping}
+              />
+            );
+          })}
+        </div>
+      </div>
+    );
   }
 }
+
 export default FlowerList;
---
+++ src/Pages/FlowerList/ListDatas.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./ListDatas.scss";
+
+class ListDatas extends React.Component {
+  render() {
+    //console.log(this.props.productImg);
+    const {
+      id,
+      itemComment,
+      prouductTitle,
+      price,
+      isDiscount,
+      discountPer,
+      resultPrice,
+      productLimit,
+      size,
+      updateDate,
+      productImg,
+      isFreeShipping,
+    } = this.props;
+    return (
+      <div className="listDatas">
+        <p className="id">{id}</p>
+        <p className="productImg">
+          <img alt="list" src={productImg} />
+        </p>
+        <p className="itemComment">{itemComment}</p>
+        <p className="prouductTitle">{prouductTitle}</p>
+        {isDiscount && (
+          <span className="detailDiscount">
+            <span className="discountPer">{discountPer}</span>
+            <span className="price">{price}</span>
+          </span>
+        )}
+        <span className="resultPrice">{resultPrice}</span>
+        <p className="productLimit">{productLimit}</p>
+        <p className="size">{size}</p>
+        <p className="updateDate">{updateDate}</p>
+        {isFreeShipping && <p className="isFreeShipping">무료배송</p>}
+      </div>
+    );
+  }
+}
+
+export default ListDatas;
