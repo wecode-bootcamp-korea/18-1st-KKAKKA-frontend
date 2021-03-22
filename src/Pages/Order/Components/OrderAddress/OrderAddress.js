8b5a3ab46e5ef8b40806b520a8671c344a54656d
sunaaank <suna.namgung@gmail.com>
2021-03-23 08:08:44 +0900

    Add : Order 최상위 컴포넌트 레이아웃 완료, 탭 기능 구현 완료

---
+++ src/Pages/Order/Components/OrderAddress/OrderAddress.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import "./OrderAddress.scss";
+
+class OrderAddress extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+export default OrderAddress;
---
+++ src/Pages/Order/Components/OrderPay/OrderPay.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import "./OrderPay.scss";
+
+class OrderPay extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+export default OrderPay;
