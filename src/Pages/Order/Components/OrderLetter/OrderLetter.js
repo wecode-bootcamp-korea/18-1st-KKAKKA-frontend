6359072a77a1b9e9e95dd5b9a26ebadc732d4428
sunaaank <suna.namgung@gmail.com>
2021-03-23 19:40:37 +0900

    Add : Order 페이지 탭1 레이아웃 구현 완료

--- src/Pages/Order/Components/OrderLetter/OrderLetter.js
+++ src/Pages/Order/Components/OrderLetter/OrderLetter.js
@@ -8,19 +8,27 @@ class OrderLetter extends Component {
         <h4>메세지카드</h4>
         <div className="letterBox">
           <div className="letterHeader">
-            <input className="letterTag">폴인로즈 에디션</input>
-            <input className="noText">메세지 없이 카드만 받을게요.</input>
+            <input type="text" className="letterTag" value="폴인로즈 에디션" />
+            <div className="noTextBox">
+              <input type="checkbox" className="noText" />
+              <p>메세지 없이 카드만 받을게요.</p>
+            </div>
           </div>
           <div className="letterBody">
             <div className="inputLetter">
-              <textarea className="text"></textarea>
-              <p>*이모티콘은 편지 내용에 포함되지 않습니다.</p>
-              <p>*편지 내용을 이곳에 직접 입력해주세요.</p>
-              <p>*붙여넣기 사용 시 편지가 입력되지 않습니다.</p>
+              <textarea className="text" placeholder="여기에 입력하세요 :-)" />
+              <div className="textInfo">
+                <span className="textLimit">0/120</span>
+                <p>*이모티콘은 편지 내용에 포함되지 않습니다.</p>
+                <p>*편지 내용을 이곳에 직접 입력해주세요.</p>
+                <p>*붙여넣기 사용 시 편지가 입력되지 않습니다.</p>
+              </div>
             </div>
             <div className="previewLetter">
-              <textarea className="textview"></textarea>
-              <p>*실제 편지지 모습입니다. 최대 8줄까지만 인쇄됩니다.</p>
+              <textarea className="textview" />
+              <div className="textviewInfo">
+                <p>*실제 편지지 모습입니다. 최대 8줄까지만 인쇄됩니다.</p>
+              </div>
             </div>
           </div>
         </div>
