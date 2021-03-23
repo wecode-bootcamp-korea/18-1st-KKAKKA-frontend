93074ef3b4656f1e2e57c679de8236dc65d32658
Geunhwa Lee <geunhwa.lee.01@gmail.com>
2021-03-23 11:31:34 +0900

    Add: 백엔드와 소통중

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router";
 import { Link } from "react-router-dom";
+import { loginAPI } from "../../config";
 import "./Login.scss";
 
 class Login extends Component {
@@ -34,10 +35,8 @@ class Login extends Component {
     }
   };
 
-  checkValidation() {}
-
-  goToMain = () => {
-    this.props.history.push("/main");
+  goToMain = e => {
+    // this.props.history.push("/main");
     // 추후 백엔드 데이터를 받으면 아래 함수 사용 예정
     /* if(response.message === "valid user") {
       this.props.history.push('/main') 
@@ -45,6 +44,29 @@ class Login extends Component {
       alert ("존재하지 않는 계정입니다. 가입 후 이용해주세요!")
       this.props.history.push('/signup')
     }*/
+
+    fetch(loginAPI, {
+      method: "POST",
+      body: JSON.stringify({
+        id: this.state.email,
+        password: this.state.password,
+      }),
+    })
+      .then(response => response.json())
+      .then(result => {
+        console.log(result);
+        if (result.message === "success_signin") {
+          this.props.history.push("/main");
+        } else if (result.message === "error_password_matching") {
+          alert("아이디 또는 비밀번호를 확인해주세요.");
+        }
+      });
+    e.preventDefault();
+  };
+
+  checkInfoLogin = () => {
+    this.isEmailValid();
+    this.goToMain();
   };
 
   render() {
@@ -75,7 +97,7 @@ class Login extends Component {
               </div>
             )}
             <div className="inputBtnBuffer"></div>
-            <button className="loginBtn" onClick={this.isEmailValid}>
+            <button className="loginBtn" onClick={this.goToMain}>
               로그인
             </button>
             <div className="signUpArea">
---
+++ src/config.js
@@ -0,0 +1,1 @@
+export const loginAPI = "http://10.58.6.0:8000/kkakka/signin";
