e5d89f1806660437420d11cfcd14389c1bb57dfb
Geunhwa Lee <geunhwa.lee.01@gmail.com>
2021-03-24 10:41:27 +0900

    Modified: nav, subnav 컴포넌트 및 common.scss  폰트 수정(팀원 협의 완)

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Nav from "../../Components/Nav/Nav";
 import SubNav from "../../Components/SubNav/SubNav";
 import Footer from "../../Components/Footer/Footer";
+import "../../styles/common.scss";
 import "./Main.scss";
 
 class Main extends Component {
