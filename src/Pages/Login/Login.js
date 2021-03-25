9d42587fa92cfe262ff5bbe21be0534338689dbe
Geunhwa Lee <geunhwa.lee.01@gmail.com>
2021-03-25 21:38:04 +0900

    Modified: 로그인 페이지 공통 컴포넌트 적용

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router";
 import { Link } from "react-router-dom";
+import Nav from "../../Components/Nav/Nav";
+import SubNav from "../../Components/SubNav/SubNav";
 import Footer from "../../Components/Footer/Footer";
 import { loginAPI } from "../../config";
 import "./Login.scss";
@@ -60,6 +62,8 @@ class Login extends Component {
     const { isValid } = this.state;
     return (
       <div className="login">
+        <Nav />
+        <SubNav />
         <div className="loginBox">
           <h1 className="loginText">로그인</h1>
           <form className="loginField">
