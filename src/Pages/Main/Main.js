1e90b71dcdda74c9afbbe42645793d384efd8c82
Geunhwa Lee <geunhwa.lee.01@gmail.com>
2021-03-25 18:59:10 +0900

    Modified: 백엔드와 통신 후 key 값 등 수정

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -26,18 +26,18 @@ class Main extends Component {
   //mainAPI는 수정 되어서 구현할 떄는 mockdata 변수로 다시 할당
   // 백엔드와 통신은 완료
   componentDidMount = () => {
-    fetch(mockCookieDataAPI)
+    fetch(mainProductAPI)
       .then(response => response.json())
       .then(result => {
         this.setState({
           cookieData: result.product_list,
         });
       });
-    fetch(mockCookieSubscribeAPI)
+    fetch(mainSubscribeAPI)
       .then(response => response.json())
-      .then(result => {
+      .then(({ result }) => {
         this.setState({
-          cookieSubscribe: result.subscription_list,
+          cookieSubscribe: result,
         });
       });
   };
@@ -57,7 +57,7 @@ class Main extends Component {
               <span className="subscribeBold">쿠키 정기구독</span>
             </h2>
             <div class="subscribeItemBox">
-              {cookieSubscribe.map(subscribeList => {
+              {cookieSubscribe?.map(subscribeList => {
                 return (
                   <Cards
                     key={subscribeList.id}
