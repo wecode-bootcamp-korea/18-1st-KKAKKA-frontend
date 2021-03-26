b721712b87ee1ca43f692ce8b3b5c9b155d9b5e1
sunaaank <suna.namgung@gmail.com>
2021-03-26 09:30:45 +0900

    Add : 백엔드 AWS 연결

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 export const config = {
-  api: "http://192.168.200.107:8000",
+  api: "http://13.125.156.179:8000",
 };
-export const mainSubscribeAPI = "10.58.3.125:8000/subscription";
+export const mainSubscribeAPI = "http://13.125.156.179:8000/subscription";
 export const mockAPI = "http://localhost:3000/data/FlowerList.json";
-export const mainProductAPI = "http://10.58.6.0:8000/product/main";
-export const loginAPI = "http://10.58.6.0:8000/kkakka/signin";
-export const signupAPI = "http://10.58.6.0:8000/kkakka/signup";
+export const mainProductAPI = "http://13.125.156.179:8000/product/main";
+export const loginAPI = "http://13.125.156.179:8000/kkakka/signin";
+export const signupAPI = "http://13.125.156.179:8000/kkakka/signup";
 export const mockCookieDataAPI = "http://localhost:3000/data/cookieData.json";
 export const mockCookieSubscribeAPI = "./data/cookieSubscribe.json";
